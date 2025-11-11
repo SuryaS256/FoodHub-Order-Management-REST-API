b25d9b8b48b27fdc551e09b3e373db5523b3f327
Surya S <surya.smg256@gmail.com>
2025-11-11 10:51:46 +0530

    Add files via upload

---
+++ FoodHubOrderApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.foodhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHubOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodHubOrderApiApplication.class, args);
+    }
+}
